0dacf3723f2e05413c78c11ae9233b5aeaed4e2e
Ryan Weaver <ryan@thatsquality.com>
2016-07-23 02:31:17 -0400

    Webpackdev: Proper Symfony parameter setup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,5 +1,6 @@
 <?php
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -51,5 +52,15 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+
+        $loader->load(function(ContainerBuilder $container) {
+            if ($container->getParameter('use_webpack_dev_server')) {
+                $container->loadFromExtension('framework', [
+                    'assets' => [
+                        'base_url' => 'http://localhost:8080'
+                    ]
+                ]);
+            }
+        });
     }
 }
