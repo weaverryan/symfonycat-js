3b49a587c3e05ec5cc9614abbc4aa901bfd87f58
Ryan Weaver <ryan@thatsquality.com>
2016-07-23 02:31:21 -0400

    React: Passing array prop & render

--- web/js/Components/ProductApp.js
+++ web/js/Components/ProductApp.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import _ from 'lodash';
 
 var ProductApp = React.createClass({
+
+
     render: function() {
+        var productRows = [];
+        _.each(this.props.initialProducts, function(product, index) {
+            productRows.push(
+                <tr key={index}>
+                    <td>{product}</td>
+                    <td className="product-price">{Math.round(Math.random()*50)}</td>
+                </tr>
+            );
+        });
+
         return (
-            <h1>{this.props.message}</h1>
+            <div>
+                <h1>{this.props.message}</h1>
+
+                <table className="table">
+                    <tbody>{productRows}</tbody>
+                </table>
+            </div>
         )
     }
 });
--- web/js/productApp.js
+++ web/js/productApp.js
@@ -1,21 +1,20 @@
-import ProductCollection from './ProductCollection';
 import '../css/productApp.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductApp  from './Components/ProductApp';
 
-var collection = new ProductCollection([
+var startingProducts = [
     'Sheer Shears',
     'Wool Hauling Basket',
     'After-Shear (Fresh Cut Grass)',
     'After-Shear (Morning Dew)',
     'Shear Comb',
     'Shearly Conditioned'
-]);
+];
 
 $(document).ready(function() {
     ReactDOM.render(
-        <ProductApp message="Great Products!" />,
+        <ProductApp message="Great Products!" initialProducts={startingProducts} />,
         document.getElementById('product-app')
     );
 });
