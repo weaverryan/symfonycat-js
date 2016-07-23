e80f8c6b93310176243ad983ffcf24d4420d5868
Ryan Weaver <ryan@thatsquality.com>
2016-07-23 02:31:12 -0400

    Basic: Setup a boring JS file

---
+++ web/js/productApp.js
@@ -0,0 +1,18 @@
+var products = [
+    'Sheer Shears',
+    'Wool Hauling Basket',
+    'After-Shear (Fresh Cut Grass)',
+    'After-Shear (Morning Dew)',
+    'Shear Comb',
+    'Shearly Conditioned'
+];
+
+var loopThroughProducts = function(callback) {
+    for (var i = 0, length = products.length; i < length; i++) {
+        callback(products[i]);
+    }
+};
+
+loopThroughProducts(function(product) {
+    console.log('Product: '+product);
+});
