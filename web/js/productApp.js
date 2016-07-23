d0f05d218fa757c409d08503ac28c5e4002446b9
Ryan Weaver <ryan@thatsquality.com>
2016-07-23 02:31:13 -0400

    ES2015: Classes

--- web/js/productApp.js
+++ web/js/productApp.js
@@ -1,15 +1,30 @@
-var products = [
+class ProductCollection
+{
+    constructor(products) {
+        this.products = products;
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProduct(i) {
+        return this.products[i];
+    }
+}
+
+var collection = new ProductCollection([
     'Sheer Shears',
     'Wool Hauling Basket',
     'After-Shear (Fresh Cut Grass)',
     'After-Shear (Morning Dew)',
     'Shear Comb',
     'Shearly Conditioned'
-];
+]);
 
 var loopThroughProducts = function(callback) {
-    for (let i = 0, length = products.length; i < length; i++) {
-        callback(products[i]);
+    for (let i = 0, length = collection.getProducts().length; i < length; i++) {
+        callback(collection.getProduct(i));
     }
 };
 
